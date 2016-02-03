5c0f66d5b53c722da7472121747268dd95334523
AndZp <andrey.oleynik.zp@gmail.com>
2016-02-03 18:41:00 +0200

    03/02 Init Commit

---
+++ flights/src/main/java/ua/com/ukrelektro/flights/HelloGreeting.java
@@ -0,0 +1,20 @@
+package ua.com.ukrelektro.flights;
+
+public class HelloGreeting {
+
+  public String message;
+
+  public HelloGreeting() {};
+
+  public HelloGreeting(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
